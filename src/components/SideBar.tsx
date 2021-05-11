579a11cbb6d16847f980bd660f62b724bd331bd1
Elvis Felix <elvisjaspion@gmail.com>
2021-05-11 03:00:22 -0300

    Finishing Challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface Genre{
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps{
+  selectedGenreId:number;
+  setSelectedGenreId(id:number):void;
+  genres: Genre[];
+}
+
+
+
+export function SideBar(props: SideBarProps) {
+  function handleClickButton(id: number) {
+  props.setSelectedGenreId(id);
+}
+
+return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre: { id: number; title: string; name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'; }) => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={props.selectedGenreId === genre.id}
+          />)
+        )}
+      </div>
+    </nav>
+  )
 }
